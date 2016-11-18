97febe9b1fe7b6f22c84df628e5b39c7669ca5b1
wangtianqi19960702 <15846505276@163.com>
2016-11-18 10:08:39 +0600

    hahaha

---
+++ lll/victor.h
@@ -0,0 +1,32 @@
+﻿#pragma once //����������� ����� ���� ���
+#include <iostream>
+using namespace std;
+/*
+struct - �������� �����
+Vector - ��� ������(���������)
+float re; - ����
+void setRe(float re); - ������
+*/
+//class - ���� �� ��������� ��������� (private)
+//struct - ���� �� ��������� ��������� (public)
+class Vector {
+private:
+	int dimension;//�����������
+	float* data;
+public:
+	Vector(int dim, float def); //����������� (����������� / �������� �� ���������)
+	~Vector(); // ���������� (�����, ������� ���������� ����� ��������� �������)
+	Vector operator+(const Vector& other);
+	Vector operator-(const Vector& other);
+	Vector operator*(float other);
+	float operator*(const Vector& other);
+	int getDimension(); // �����, ������������ ����������� �������
+	float& operator[](int); // �������� ������� � ������
+};
+
+Vector operator*(float re, Vector& c);
+
+istream& operator>>(istream& is, Vector& c);
+ostream& operator<<(ostream& os, Vector& c);
+
+
